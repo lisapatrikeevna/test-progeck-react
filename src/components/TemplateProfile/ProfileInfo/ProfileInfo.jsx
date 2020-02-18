a2efd9c18b851cbbc6357ffbc8105011e3ad57b7
lisapatrikeevna <38157157+lisapatrikeevna@users.noreply.github.com>
2020-02-19 01:57:39 +0200

    Add files via upload

---
+++ src/components/TemplateProfile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,19 @@
+import React  from 'react';
+import cl from'./ProfileInfo.module.css';
+
+
+const ProfileInfo = () => {
+    return (
+        <div className = 'ava-desc d-flex'>
+            <img src='http://webstydio.lisa15.ru/image/cache/catalog/demo/product/juridicheskoesoprovozhdenie2-74x74.jpg'/>
+                <p>
+                    Name & 
+                </p>
+                <p>
+                    last Name
+                </p>
+        </div>
+    );
+}
+
+export default ProfileInfo;
