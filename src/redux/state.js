ec83ca01629e18ebbfe92a1b2ef832f96a1e34fb
lisapatrikeevna <38157157+lisapatrikeevna@users.noreply.github.com>
2020-03-04 02:56:01 +0200

    Add files via upload

--- src/redux/state.js
+++ src/redux/state.js
@@ -9,24 +9,45 @@ let state = {
             {id: 3, message: 'yo', likesCount: 12},
             {id: 0, message: 'it\`s my first post', likesCount: 9}
         ],
-        // newPostText:['']
+         newPostText:['enter youre text']
     },
     pointMessages: {
         dialogItem: [
-           {id: 0, name: 'Kat'} ,
+           {id: 0, name: 'Kat' } ,
            {id: 1, name:'Dan'},
            {id: 2, name:'Lisa'}
+        ],
+        messageData: [
+            {id: 0, message: 'hi'},
+            {id: 1, message: 'are you besy?'},
+            {id: 2, message: 'yes'}
         ]
     },
     sidbar:{}
 }
-export let addPost = (postLastMessage) => {
-    let newPost = {
-        id: 4,
-        message: postLastMessage,
-        likesCount: 0
-    };
-    state.pointBlog.postMessage.push(newPost);
+// export let addPost = (postLastMessage) => {
+//     let newPost = {
+//         id: 4,
+//         message: postLastMessage,
+//         likesCount: 0
+//     };
+//     state.pointBlog.postMessage.push(newPost);
+//     rerenderEntireTree(state);
+// }
+export let addPost = () => {
+        let newPost = {
+            id: 4,
+            message: state.pointBlog.newPostText,
+            likesCount: 0
+        };
+        state.pointBlog.postMessage.push(newPost);
+        state.pointBlog.newPostText = '';
+
+        rerenderEntireTree(state);
+    }
+export let updateNewPostText = (newText) => {
+   
+    state.pointBlog.newPostText = newText;
     rerenderEntireTree(state);
 }
 
