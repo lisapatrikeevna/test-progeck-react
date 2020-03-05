8f161c526bee2bb644ccc114fa28a92537fcf117
lisapatrikeevna <38157157+lisapatrikeevna@users.noreply.github.com>
2020-03-06 01:50:58 +0200

    Update render.js

--- src/render.js
+++ src/render.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import {BrowserRouter, Route} from "react-router-dom";
 import App from './App';
-import {addPost} from './redux/state.js';
+import {addPost,updateNewPostText} from './redux/state.js';
 // import state from './redux/state';
 
 
 export let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost}/>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
         </BrowserRouter>, document.getElementById('root'));
 }
-
