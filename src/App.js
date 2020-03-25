117bf4d40cafaae23c10ed514c6f641aac1e5b13
lisapatrikeevna <38157157+lisapatrikeevna@users.noreply.github.com>
2020-03-25 07:36:27 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,27 +4,24 @@ import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
 import Footer from './components/Footer';
-import Dialogs from './components/Dialogs(0)';
+import Dialogs from './components/Dialogs';
 import News from './components/News';
 import Music from './components/Music';
 import Settings from './components/Settings';
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
+import DialogsWrap from './components/DialogsWrep';
 
 const App = (props) => {
-
+// debugger
   return (
       <div className='app-wrapper'>
         <Header/>
         <Navbar />
         <div  className = 'wrappContent'>
           <Route path ='/dialogs' 
-           render ={ () => <Dialogs  store={props.store} />  } />
+           render ={ () => <DialogsWrap  store={props.store} />  } />
           <Route path ='/profile' 
-           render = { () => <Profile 
-            pointBlog={props.state.pointBlog}
-            dispatch={props.dispatch} 
-              />
-          }/>
+           render = { () => <Profile store = {props.store} />  }/>
           <Route path = '/news' component = { News }/>
           <Route path ='/music' component = { Music}/>
           <Route path ='/settings' component ={ Settings}/>
--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter, Route} from "react-router-dom";
-// import {addPost, updateNewPostText, subscribe} from './redux/state';
+import {BrowserRouter} from "react-router-dom";
 import store from './redux/redux-store';
-// import { addPost, updateNewPostText } from './redux/state';
-// import {rerenderEntireTree} from '../components/render';
-// import {rerenderEntireTree} from "./render";
-
-
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
@@ -21,7 +15,12 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+// store.subscribe(rerenderEntireTree);
+
+ store.subscribe(() => {
+    let state = store.getState();
+    rerenderEntireTree(state);
+ });
 //ReactDOM.render(<App postMessage={postMessage} dialogItem={dialogItem} messageData={messageData} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
